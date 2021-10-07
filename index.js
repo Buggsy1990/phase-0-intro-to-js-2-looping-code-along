ca7c9505104436ddc491df63ad4df69f6a59fb27
buggsy1106@gmail.com <buggsy1106@gmail.com>
2021-10-07 14:15:34 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+function writeCards(names) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+  }
+  return messages;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num -= 1;
+  }
+}
+countDown(4);
